a919046c440a3df62bbcf74306723414b05d1e03
AigaGoto <s1811337@u.tsukuba.ac.jp>
2021-09-04 11:28:38 +0900

    finished to p31

--- p16.cpp
+++ p16.cpp
@@ -7,10 +7,20 @@ int main() {
     int N;
     cin >> N;
 
-    int a[N];
+    int a;
+    long sum = 0;
+    int min = 1000000;
+    int max = -1000000;
 
     for(int i=0; i < N; i++) {
-        
+        cin >> a;
+        if (a < min) min = a;
+        if (a > max) max = a;
+        sum += a;
     }
+
+    cout << min << ' ' << max << ' ' << sum << endl;
+
+    return 0;
 
 }
